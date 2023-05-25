63ad3d94071b6f948f47072c9ed7c5ae34f32f4b
MattiaFezzardi <mattiafezzardi1993@gmail.com>
2023-05-25 18:04:17 +0200

    projects stampati correttamente in console

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,43 @@
 <script>
+import axios from 'axios';
 import AppHeader from './components/AppHeader.vue';
 import AppMain from './components/AppMain.vue';
 import AppFooter from './components/AppFooter.vue';
 
 export default {
     name: 'App',
+
+    data() {
+      return {
+        apiBaseUrl: 'http://127.0.0.1:8000/api',
+        apiUrls: {
+          projects: '/projects'
+        },
+        projects:[]
+      }
+    },
+
     components: {
         AppHeader,
         AppMain,
         AppFooter
+    },
+
+    methods: {
+      getProjects() {
+        axios.get(this.apiBaseUrl + this.apiUrls.projects)
+        .then((response) => {
+          this.projects = response.data.results;
+          console.log(response);
+        })
+        .catch((error) => {
+          console.log(error);
+        })
+      }
+    },
+
+    created() {
+      this.getProjects();
     }
 }
 </script>
